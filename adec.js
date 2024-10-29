19d02d34b734a8195721fc621e6799f346f8acb4
adec1B <adec16b@gmail.com>
2024-10-29 14:54:07 -0300

    Add files via upload

---
+++ adec.js
@@ -0,0 +1,5 @@
+// Exemplo de funcionalidade JavaScript
+document.addEventListener("DOMContentLoaded", function() {
+    // Código que pode ser adicionado para interatividade, se necessário
+    console.log("Página carregada com sucesso!");
+});
